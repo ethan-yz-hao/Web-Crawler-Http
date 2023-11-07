7d1b18ddee8a748798f94a10cd1eded3faf18067
Ethan Hao <hao.yz.ethan@gmail.com>
2023-11-07 16:42:38 -0500

    crawl pages

--- crawl.js
+++ crawl.js
@@ -1,27 +1,47 @@
 const {JSDOM} = require('/home/asus/.nvm/versions/node/v18.7.0/lib/node_modules/jsdom')
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
+
     console.log(`actively crawling: ${currentURL}`)
 
     try {
         const resp = await fetch(currentURL)
-
         if (resp.status > 399) {
             console.log(`error in fetch with status code: ${resp.status}, on page: ${currentURL}`)
-            return
+            return pages
         }
 
         const contentType = resp.headers.get("content-type")
-
         if (!contentType.includes("text/html")) {
-            console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`)
-            return
+            console.log(`got non html response, content type: ${contentType}, on page: ${currentURL}`)
+            return pages
         }
 
-        console.log(await resp.text())
+        var htmlBody = await resp.text()
+
     } catch (err) {
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+    for (const nextURL of nextURLs) {
+        pages = await crawlPage(baseURL, nextURL, pages)
+    }
+
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const {crawlPage} = require('./crawl.js')
 
-function main() {
+async function main() {
     if (process.argv.length < 3) {
         console.log("no website provided")
         process.exit(1)
@@ -10,8 +10,12 @@ function main() {
         process.exit(1)
     }
     const baseURL = process.argv[2]
+
     console.log("starting crawl")
-    crawlPage(baseURL)
+    const pages = await crawlPage(baseURL, baseURL, {})
+    for (const page of Object.entries(pages)) {
+        console.log(page)
+    }
 }
 
 main()
