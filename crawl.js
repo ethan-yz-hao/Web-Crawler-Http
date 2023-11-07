af915890eb52df65694b41bb08e47eefeac5c212
Ethan Hao <hao.yz.ethan@gmail.com>
2023-11-06 22:45:03 -0500

    get url

--- crawl.js
+++ crawl.js
@@ -1,3 +1,32 @@
+const {JSDOM} = require('/home/asus/.nvm/versions/node/v18.7.0/lib/node_modules/jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
+            // relative URL
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(`${baseURL}${linkElement.href}`)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`)
+            }
+
+        } else {
+            // absolute URL
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(linkElement.href)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostpath = `${urlObj.hostname}${urlObj.pathname}`
@@ -9,5 +38,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
